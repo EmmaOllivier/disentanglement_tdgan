d8009cf30ae1c48ff8b88559bf821649f03c4854
EmmaOllivier <86803985+EmmaOllivier@users.noreply.github.com>
2024-08-14 14:55:47 -0400

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -9,8 +9,11 @@ from tqdm import tqdm
 import PIL
 
 
+
+
+
 class Dataset_Biovid_image_binary_class(torch.utils.data.Dataset):
-    def __init__(self,PATH_IMG,PATH_ANOT,transform = lambda x: x,IDs = None,set_type = None,nb_image = None,nb_fold=1,preload=False,loaded_resolution=224, seed=42):
+    def __init__(self,PATH_IMG,PATH_ANOT,transform = lambda x: x,IDs = None,set_type = None,nb_image = None,preload=False,loaded_resolution=224, seed=42):
         self.PATH_IMG = PATH_IMG
         self.PATH_ANOT = PATH_ANOT
         self.transform = transform
@@ -20,9 +23,6 @@ class Dataset_Biovid_image_binary_class(torch.utils.data.Dataset):
         self.preload = preload
         self.seed = seed
         self.reset()
-        q_fold = 40//nb_fold
-        self.fold = [[j for j in range(i*q_fold,(i+1)*q_fold)] for i in range(nb_fold)]
-
         dataframe = pd.read_csv(self.PATH_ANOT)
         N = len(dataframe)
         self.resize  = torchvision.transforms.Resize((loaded_resolution,loaded_resolution),antialias=True)
@@ -39,14 +39,9 @@ class Dataset_Biovid_image_binary_class(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.index_link)
     
-    def reset(self,fold=None,keep=False):
+    def reset(self):
         dataframe = pd.read_csv(self.PATH_ANOT)
         self.dataframe = dataframe
-        if not fold is None and fold < len(self.fold) and fold >=0:
-            if not keep:
-                self.dataframe = self.dataframe[~ self.dataframe['id_video'].isin(self.fold[fold])]
-            else:
-                self.dataframe = self.dataframe[self.dataframe['id_video'].isin(self.fold[fold])]
 
         self.dic_ID = {d : i for i,d in enumerate(set(self.dataframe['ID']))}
         if not self.nb_image is None:
@@ -74,10 +69,10 @@ class Dataset_Biovid_image_binary_class(torch.utils.data.Dataset):
         ID_tensor = self.dic_ID[self.dataframe.loc[index,'ID']]
         video_tensor = self.dataframe.loc[index,'id_video']
         
-        return img_tensor, pain_tensor, ID_tensor
+        return img_tensor, pain_tensor, ID_tensor, video_tensor
 
 class Dataset_Biovid_image_class(torch.utils.data.Dataset):
-    def __init__(self,PATH_IMG,PATH_ANOT,transform = lambda x: x,IDs = None,set_type = None,nb_image = None,nb_fold=1,preload=False,loaded_resolution=224, seed=42):
+    def __init__(self,PATH_IMG,PATH_ANOT,transform = lambda x: x,IDs = None,set_type = None,nb_image = None,preload=False,loaded_resolution=224, seed=42):
         self.PATH_IMG = PATH_IMG
         self.PATH_ANOT = PATH_ANOT
         self.transform = transform
@@ -87,8 +82,6 @@ class Dataset_Biovid_image_class(torch.utils.data.Dataset):
         self.preload = preload
         self.seed=seed
         self.reset()
-        q_fold = 40//nb_fold
-        self.fold = [[j for j in range(i*q_fold,(i+1)*q_fold)] for i in range(nb_fold)]
 
         dataframe = pd.read_csv(self.PATH_ANOT)
         N = len(dataframe)
@@ -107,14 +100,9 @@ class Dataset_Biovid_image_class(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.index_link)
     
-    def reset(self,fold=None,keep=False):
+    def reset(self):
         dataframe = pd.read_csv(self.PATH_ANOT)
         self.dataframe = dataframe
-        if not fold is None and fold < len(self.fold) and fold >=0:
-            if not keep:
-                self.dataframe = self.dataframe[~ self.dataframe['id_video'].isin(self.fold[fold])]
-            else:
-                self.dataframe = self.dataframe[self.dataframe['id_video'].isin(self.fold[fold])]
 
         self.dic_ID = {d : i for i,d in enumerate(set(self.dataframe['ID']))}
         if not self.nb_image is None:
@@ -147,7 +135,7 @@ class Dataset_Biovid_image_class(torch.utils.data.Dataset):
 class DualTrainDataset(torch.utils.data.Dataset):
 
 
-    def __init__(self,PATH_IMG,PATH_ANOT1,PATH_ANOT2,transform = lambda x: x,IDs = None,set_type = None,nb_image = None,nb_fold=1,preload=False,loaded_resolution=224, seed=42):
+    def __init__(self,PATH_IMG,PATH_ANOT1,PATH_ANOT2,transform = lambda x: x,IDs = None,set_type = None,nb_image = None, preload=False,loaded_resolution=224, seed=42):
         self.PATH_IMG = PATH_IMG
         self.PATH_ANOT1 = PATH_ANOT1
         self.PATH_ANOT2 = PATH_ANOT2
@@ -159,8 +147,6 @@ class DualTrainDataset(torch.utils.data.Dataset):
         self.seed=seed
         
         self.reset()
-        q_fold = 40//nb_fold
-        self.fold = [[j for j in range(i*q_fold,(i+1)*q_fold)] for i in range(nb_fold)]
         
         dataframe1 = pd.read_csv(self.PATH_ANOT1)
         dataframe2 = pd.read_csv(self.PATH_ANOT2)
@@ -183,14 +169,9 @@ class DualTrainDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.index_link1)
     
-    def reset(self,fold=None,keep=False):
+    def reset(self):
         dataframe1 = pd.read_csv(self.PATH_ANOT1)
         self.dataframe1 = dataframe1
-        if not fold is None and fold < len(self.fold) and fold >=0:
-            if not keep:
-                self.dataframe1 = self.dataframe1[~ self.dataframe1['id_video'].isin(self.fold[fold])]
-            else:
-                self.dataframe1 = self.dataframe1[self.dataframe1['id_video'].isin(self.fold[fold])]
 
         self.dic_ID = {d : i for i,d in enumerate(set(self.dataframe1['ID']))}
         if not self.nb_image is None:
@@ -200,11 +181,6 @@ class DualTrainDataset(torch.utils.data.Dataset):
 
         dataframe2 = pd.read_csv(self.PATH_ANOT2)
         self.dataframe2 = dataframe2
-        if not fold is None and fold < len(self.fold) and fold >=0:
-            if not keep:
-                self.dataframe2 = self.dataframe2[~ self.dataframe2['id_video'].isin(self.fold[fold])]
-            else:
-                self.dataframe2 = self.dataframe2[self.dataframe2['id_video'].isin(self.fold[fold])]
         if not self.nb_image is None:
             self.index_link2 = list(self.dataframe2.sample(self.nb_image, random_state=self.seed).index)
         else:
@@ -214,8 +190,6 @@ class DualTrainDataset(torch.utils.data.Dataset):
         seeds=[]
         for i in range(nb_seeds):
             seeds.append(i)
-            if(i==0):
-                print(i)
         return seeds
 
 
@@ -242,12 +216,17 @@ class DualTrainDataset(torch.utils.data.Dataset):
         index2 = random.randint(0, len(self.dataframe2)-1)
         #print(index2)
         while(self.dataframe2.loc[index2,'pain']==self.dataframe1.loc[index,'pain']):
+                #print(self.dataframe1.loc[index,'pain'])
                 index2=random.randint(0, len(self.dataframe2)-1)
+                #print(index2)
 
         try :
             i = self.dic_image2[index2]
         except:
 
+            #while(self.dataframe2.loc[index2,'pain']==self.dataframe1.loc[index,'pain']):
+                #print(self.dataframe1.loc[index,'pain'])
+                #index2=random.randint(0, len(self.dataframe2)-1)
             i = len(self.dic_image2)
             self.dic_image2[index2] = i
             try:
